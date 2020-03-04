d09598d66ddb5e3f64c70663f89acb2ddcf692f4
new <1054694788@qq.com>
2020-03-04 08:01:40 +0800

    e

--- lab1/linux/in.c
+++ lab1/linux/in.c
@@ -2,3 +2,25 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/sysmacros.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    if (mknod("./ttyS0", S_IFCHR | S_IRUSR | S_IWUSR, makedev(4, 64)) == -1 || 
+    mknod("./fb0", S_IFCHR | S_IRUSR | S_IWUSR, makedev(29, 0)) == -1)
+    {
+        perror("mknod() failed");
+    }
+    */
+    if(system("./1")==-1||system("./2")==-1||system("./3")==-1)
+    {
+        printf("failed");
+    }
+    printf("Hello\n");
+    return 0;
+}
